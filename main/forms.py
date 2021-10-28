3220fe0d358c34d023ff22a605c55114519ee29a
MD Suhaib Safwan <suhaibsafwan45@gmail.com>
2021-10-28 23:39:32 +0600

    validation in forms and signals

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Content, UserProfile
 
+# Validation check --> 1. Object level validation, 2. Full validation
+
 
 class ContentForm(forms.ModelForm):
 
@@ -8,7 +10,13 @@ class ContentForm(forms.ModelForm):
         model = Content
         fields = "__all__"
 
-    def validation_title(self):
-        print("Validating text")
+    def clean_text(self):
+        print("Validating text inside forms")
+        text = self.cleaned_data.get("text")
+        print(text)
 
+        if "django" not in text:
+            raise forms.ValidationError("Blog is not about Django")
+
+        return text
 
