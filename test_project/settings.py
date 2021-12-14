9e154999abb0b6ea5fc18c7575b58f44a636272e
MD Suhaib Safwan <68965547+MdSuhaibSafwan@users.noreply.github.com>
2021-12-14 09:06:52 -0800

    modified settings

--- test_project/settings.py
+++ test_project/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "blogtestproject.herokuapp.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
